50d48e934448832a7f046cf72f2a41af4f5473ed
Geoff Bremner <geoff.bremner@indexexchange.com>
2015-10-29 21:25:28 -0400

    arbitrary vote options supported and mongo DB used

--- votebot.js
+++ votebot.js
@@ -1,8 +1,12 @@
 var request = require('request');
-var vote = {};
+var uuid = require('node-uuid');
 var botPayload = {};
 	botPayload.username = 'votebot';
 	botPayload.icon_emoji = ':hand:';
+var mongodb = require('mongodb');
+var assert = require('assert');
+
+var url = 'mongodb://root:index123@ds045684.mongolab.com:45684/mongoslack';
 
 module.exports = function(req, res, next) {
 	if (req.body.text) {
@@ -15,7 +19,7 @@ module.exports = function(req, res, next) {
 			endVote(req, res, next);
 		}
 		else { // vote on a vote
-			makeVote(req, res, next, params);
+			enterVote(req, res, next, params);
 		}
 	} else {
 		// send error message back to user if input is bad
@@ -25,72 +29,124 @@ module.exports = function(req, res, next) {
 
 function startVote(req, res, next) {
 	// write response message and add to payload
-	questionText = req.body.text.match(/'([^']+)'/)[1];
-
+	var questionText = req.body.text.match(/'([^']+)'/)[1];
+	var voteOptionsText = req.body.text.match(/\[(.*?)\]/)[1];
+	var voteOptionsLoopText = req.body.text.match(/\[(.*?)\]/)[1].split(' ');
+	var vote = {};
+	vote.voteID = uuid.v4();
 	vote.questionText = questionText;
-	vote.owner = req.body.user_id;
-	vote.yes = 0;
-	vote.no = 0;
-	vote.voters = [];
+	vote.ownerID = req.body.user_id;
+	vote.votes = {};
 
-	botPayload.text = req.body.user_name + " has started a vote!\n" + questionText + "\nTo vote, use '/vote {yes/no}'";
-	botPayload.channel = req.body.channel_id;
+	for (var i = 0; i < voteOptionsLoopText.length; i++) {
+		vote.votes[voteOptionsLoopText[i]] = [];
+	}
 
-	send(botPayload, function (error, status, body) {
-		if (error) {
-			return next(error);
-		} else if (status !== 200) {
-		// inform user that our Incoming WebHook failed
-			return next(new Error('Incoming WebHook: ' + status + ' ' + body));
-		} else {
-			return res.status(200).end();
-		}
+	botPayload.text = req.body.user_name + " has started a vote!\n" + questionText + "\nVote with one of the following options:\n" 
+					+ voteOptionsText + "\nVote ID = " + vote.voteID;	botPayload.channel = req.body.channel_id;
+
+	mongodb.MongoClient.connect(url, function(err, db) {
+	assert.equal(null, err);
+	console.log("Connected correctly to server");
+
+	var collection = db.collection('voting');
+		collection.insert(vote, function(err, result) {
+			console.log("Inserted documents into the document collection");
+			db.close();
+			send(botPayload, function (error, status, body) {
+				if (error) {
+					return next(error);
+				} else if (status !== 200) {
+				// inform user that our Incoming WebHook failed
+					return next(new Error('Incoming WebHook: ' + status + ' ' + body));
+				} else {
+					return res.status(200).end();
+				}
+			});
+		});
 	});
 }
 
 function endVote(req, res, next) {
-	if (Object.keys(vote).length !== 0) { // if we actually have a vote
-		if (req.body.user_id === vote.owner) {
-			botPayload.text = "Vote results to " + vote.questionText + " yes: " + vote.yes + " no: " + vote.no;
-			botPayload.channel = req.body.channel_id;
-			vote = {};
-		} else {
-			return res.status(200).send('You are not the vote owner, you cannot end this vote');
-		}
-	} else {
-		return res.status(200).send('There are no active votes!');
+	var voteID = req.body.text.split(' ')[1] || "";
+
+	if (voteID === "") {
+		return res.status(200).send('Please use /vote end {vote_ID}');
 	}
 
-	send(botPayload, function (error, status, body) {
-		if (error) {
-			return next(error);
-		} else if (status !== 200) {
-		// inform user that our Incoming WebHook failed
-			return next(new Error('Incoming WebHook: ' + status + ' ' + body));
-		} else {
-			return res.status(200).end();
-		}
+	mongodb.MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
+		console.log("Connected correctly to server");
+
+		var collection = db.collection('voting');
+		console.log(voteID);
+		console.log(req.body.user_id);
+		collection.find({voteID: voteID, ownerID: req.body.user_id}).toArray(function(err, doc) {
+			console.log(doc);
+			if (doc.length === 0) {
+				db.close();
+				return res.status(200).send('Please double check your vote ID. You cannot end a vote you did not start.');
+			} else {
+				collection.deleteOne({voteID: voteID});
+				var voteResultsText = "";
+				var vote = doc[0];
+				for (var voteOption in vote.votes) {
+					voteResultsText += voteOption + " " + vote.votes[voteOption].length + " ";
+				}
+				botPayload.text = "Vote results to " + vote.questionText + " " + voteResultsText;
+		 		botPayload.channel = req.body.channel_id;
+		 		db.close();
+		 		send(botPayload, function (error, status, body) {
+					if (error) {
+						return next(error);
+					} else if (status !== 200) {
+					// inform user that our Incoming WebHook failed
+						return next(new Error('Incoming WebHook: ' + status + ' ' + body));
+					} else {
+						return res.status(200).end();
+					}
+				});
+			}
+		});
 	});
 }
 
-function makeVote(req, res, next, params) {
+function enterVote(req, res, next, params) {
 	var voter = req.body.user_id;
-	for (var i = 0; i < vote.voters.length; i++) {
-		if (voter === vote.voters[i]) { // this user has already voted
-			return res.status(200).send('You have already voted!');
-		}
-	}
-	// valid voter
-	var votersVote = params[0];
-	if (votersVote === 'yes') {
-		vote.yes += 1;
-	} else if (votersVote === 'no') {
-		vote.no += 1;
-	} else {
-		return res.status(200).send("You voted " + votersVote + ". Please vote 'yes' or 'no'");
-	}
-	vote.voters.push(voter);
-	return res.status(200).send('You voted ' + votersVote);
+	var splitText = req.body.text.split(' ');
+	var voteID = splitText[0];
+	var voteOption = splitText[1];
+
+	mongodb.MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
+		var collection = db.collection('voting');
+		collection.find({voteID: voteID}).toArray(function(err, doc) {
+			console.log(doc);
+			if (doc.length === 0) {
+				db.close();
+				return res.status(200).send('Please double check your vote ID. You cannot vote on a vote that does not exist');
+			} else {
+				var vote = doc[0];
+				if (!vote.votes.hasOwnProperty(voteOption)) {
+					return res.status(200).send('Invalid vote option');
+				} 
+				for (var loopVoteOption in vote.votes) {
+					console.log(loopVoteOption);
+					for (var i = 0; i < vote.votes[loopVoteOption].length; i++) {
+						console.log(vote.votes[loopVoteOption]);
+						if (voter === vote.votes[loopVoteOption][i]) {
+							return res.status(200).send('You already voted: ' + loopVoteOption);
+						}
+					}
+				}
+				vote.votes[voteOption].push(voter);
+				collection.updateOne({voteID: voteID}, { $set: {votes: vote.votes} });
+				
+		 		db.close();
+		 		return res.status(200).send('You voted: ' + voteOption);
+			}
+		});
+	});
 }
 
 function send (payload, callback) {
